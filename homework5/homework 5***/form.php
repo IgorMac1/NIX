1d32cf9a9b32b8a2a0bddc834a7d9fe5738b8a7f
Ihor Makieiev <makieiev@yahoo.com>
2021-10-24 15:21:00 +0300

    homework5

---
+++ homework5/homework 5***/form.php
@@ -0,0 +1,4 @@
+<form action="index.php" method="get">
+    <input type="number" name="guess">
+    <input type="submit" [name='go'] value='Угадать'>
+</form>
---
+++ homework5/homework 5***/index.php
@@ -0,0 +1,20 @@
+<?php
+require 'form.php';
+if (isset($_GET['guess']) && $_GET['guess'] !== '') {
+    $playerNum = $_GET['guess'];
+    $randNum = rand(5, 8);
+} else echo 'Введите число';
+if ($playerNum < 5 && $playerNum !== null) {
+    echo 'Число маленькое';
+} elseif ($playerNum > 8 && $playerNum !== null) {
+    echo 'Число большое';
+} elseif ($playerNum == $randNum && $playerNum !== null){
+    echo 'Вы угадали !!!! <p>Вы угадали!!!  <a href="index.php">Играть еще!!!</a></p>';
+}
+elseif($playerNum != $randNum) {
+    echo "<p>Вы не угадали!!!  <a href='index.php'>Попробовать еще!!!</a></p>";
+}
+
+
+
+
